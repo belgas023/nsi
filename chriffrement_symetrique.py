176cf7f2a78b8772b5a63feab85d5f9c5b06ec6e
belgas023 <hugon.come@gmail.com>
2026-02-03 15:30:11 +0100

    tp1 chiffrement symetrique

--- chriffrement_symetrique.py
+++ chriffrement_symetrique.py
@@ -4,13 +4,13 @@ message = "Message secret"
 def chiffre(message, masque):
     message_crypté = [] 
     for i in range(len(message)):
-        message_crypté.append(ord(message[i]) ^ ord(masque[i]))
+        message_crypté.append( ord(message[i])^ord(masque[i]) )
     return message_crypté
 
 def dechiffre(message, masque):
     message_decrypté = ""
     for i in range(len(message)):
-        message_decrypté += chr( message[i] ^ ord(masque[i]) )
+        message_decrypté += chr( message[i]^ord(masque[i]) )
     return message_decrypté
          
 
